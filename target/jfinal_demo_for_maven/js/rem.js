a1ec19d614f4439efbfd67ea9b2441443a561ac9
jiushaba <admin@zdlog.com>
2017-09-07 13:29:03 +0800

    二次修复bug

---
+++ target/jfinal_demo_for_maven/js/rem.js
@@ -0,0 +1,46 @@
+﻿$(function () {
+    $("#btnToRem").click(function () {
+        var text = $("#txtSourceRem").val();
+        var rem = $("#txtRemIntalize").val();
+        if (text == "") {
+            error("请填写要转换的CSS");
+        }
+        else if (rem == "") {
+            error("请填写1rem等于多少px值");
+        }
+        else if (rem <= 0) {
+            error("请填写正确的px值");
+        }
+        else {
+            text = $.format(text, { method: "css" });   // 都转换成多行来比较
+            var arr = text.split("\n");
+            var sb = '';
+            for (var i = 0; i < arr.length; i++) {
+                var line = arr[i];
+                sb += line.replace(/\d+px/g, function (px) {
+                    if ($("#chkIgnoreBorder").is(":checked")) {
+                        if (!/border:/ig.test(line)) {
+                            return (parseInt(px) / parseInt(rem)) + "rem";
+                        }
+                        else {
+                            return px;
+                        }
+                    }
+                    else {
+                        return (parseInt(px) / parseInt(rem)) + "rem";
+                    }
+                }) + "\n";
+            }
+            var source = $("#txtSourceRem").val();
+            var sourceArr = source.split("\n");
+            var len = sourceArr.length;
+            if (len > 1) {
+                sb = $.format(sb, { method: "css" });
+            }
+            else {
+                sb = $.format(sb, { method: "cssmin" });
+            }
+            $("#txtDestRem").val(sb);
+        }
+    });
+});
---
+++ target/jfinal_demo_for_maven/js/upper.js
@@ -0,0 +1,21 @@
+﻿$(function () {
+    $("#btnGetUpper").click(function () {
+        var text = $("#txtSourceUpper").val();
+        if (text == "") {
+            error("请填写要转换大写的内容");
+        }
+        else {
+            $("#txtDestUpper").val(text.toUpperCase());
+        }
+    });
+
+    $("#btnGetLower").click(function () {
+        var text = $("#txtSourceUpper").val();
+        if (text == "") {
+            error("请填写要转换小写的内容");
+        }
+        else {
+            $("#txtDestUpper").val(text.toLowerCase());
+        }
+    });
+});
